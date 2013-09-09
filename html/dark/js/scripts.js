1a72299cd92cf302d457551d126f36bcc2c9007b
zaba <admin@boxbilling.com.pl>
2013-09-09 21:52:48 +0200

    Alfa Version Templat Home

---
+++ html/dark/js/scripts.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('#slides1').bxSlider({
+		prev_image: 'images/btn_arrow_left.png',
+		next_image: 'images/btn_arrow_right.png',
+		wrapper_class: 'slides1_wrap',
+		margin: 70,
+		auto: true,
+		auto_controls: true
+	});
+});
